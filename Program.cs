55967f0358e82328e267483ffafc24c546da64c1
jiratchot-68 <jiratchot.n@kkumail.com>
2025-12-02 11:05:52 +0700

    Jiratchot

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace jirat
     {
         static void Main(string[] args)
         {
+            //print "hello world" to the console
+            Console.WriteLine("hello world");
+            Console.WriteLine("my name is' jirat");
+            Console.WriteLine("studentID 683450351-7");
+            Console.WriteLine("major: computer science");
+            Console.WriteLine("faculty : interdisciplinary");
+            Console.WriteLine("Khon kaen university");
         }
     }
 }
